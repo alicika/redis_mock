e1c5b073965f2c4b1dbb831ede140fd2f2b58a7f
alicika <52126032+alicika@users.noreply.github.com>
2021-05-26 23:24:37 +0900

    implement hand-written i/o processing

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -79,17 +79,3 @@ async fn main() {
     t2.await.unwrap();
     manager.await.unwrap();
 }
-
-use tokio::fs::File;
-use tokio::io::{self, AsyncReadExt};
-
-#[tokio::main]
-async fn main() -> io::Result<()> {
-    let mut f = File::open("foo.txt").await?;
-    let mut buffer = [0; 10];
-
-    let n = f.read(&mut buffer[..]).await?;
-
-    println!("The bytes: {:?}", &buffer[..n]);
-    Ok(())
-}
