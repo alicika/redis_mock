0143bc149a7b719d0990c09451d8bb1f649c030d
alicika <52126032+alicika@users.noreply.github.com>
2021-05-27 23:35:20 +0900

    add framing with fixed-sized buffer

---
+++ src/bin/frame.rs
@@ -0,0 +1,34 @@
+use bytes::{BytesMut, Buf};
+use mini_redis::{Frame, Result};
+use tokio::net::TcpStream;
+use tokio::io::AsyncReadExt;
+
+pub struct Connection {
+    stream: TcpStream,
+    buffer: BytesMut,
+}
+
+impl Connection {
+    pub fn new(stream: TcpStream) -> Connection {
+        Connection {
+            stream,
+            buffer: BytesMut::with_capacity(4096),
+        }
+    }
+
+    pub async fn read_frame(&mut self) -> Result<Option<Frame>> {
+        loop {
+            if let Some(frame) = self.parse_frame()? {
+                return Ok(Some(frame));
+            }
+
+            if 0 == self.stream.read_buf(&mut self.buffer).await? {
+                if self.buffer.is_empty() {
+                    return Ok(None);
+                } else {
+                    return Err("connection reset by peer".into());
+                }
+            }
+        }
+    }
+}
